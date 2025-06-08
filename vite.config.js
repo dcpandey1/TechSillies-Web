4e3e5b38fd939a676173b9e810cc3a9c9bfcbe64
Dheeraj Pandey <dp151439@gmail.com>
2025-06-08 16:32:00 +0530

    Aadhar Verification

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,7 @@ export default defineConfig({
     allowedHosts: ["88be-2409-40f2-3005-5b19-20d0-5d59-436f-2ddd.ngrok-free.app"],
   },
   plugins: [react()],
+  optimizeDeps: {
+    include: ["html5-qrcode"], // ✅ Include it in optimization step
+  },
 });
